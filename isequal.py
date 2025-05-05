719d90cd1165f5b7f8c5169337469ff735855c68
Soujanya Maharudra Bailawad <soujanyabailawad@gmail.com>
2025-05-05 07:16:58 +0530

    Add files via upload

---
+++ isequal.py
@@ -0,0 +1,19 @@
+a=4
+b=4
+
+print(a is b) #exact loc in mem
+print( a == b) #value
+
+class Person:
+    name = "souju"
+    occ = "fd"
+    nw = 100
+    def info(self):
+        print(f"{self.name} is a {self.occ}")
+
+
+a = Person()
+print(a.name)
+a.name = "tuvi"
+print(a.name)
+a.info()
