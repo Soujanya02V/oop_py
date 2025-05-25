9255bd92e045d480ac311796796411e5e379aac5
Soujanya Maharudra Bailawad <soujanyabailawad@gmail.com>
2025-05-25 14:10:35 +0530

    Update dirdict.py

--- dirdict.py
+++ dirdict.py
@@ -1,1 +1,12 @@
-#how the directory and dictionary works in python
+
+x = [1,2,3]
+#print(dir(x))
+print(x.__add__)
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+p = Person("souju",21)
+print(p.__dict__)
+
+print(help(Person))
